826428c6634833cf3cd0b4c1cfbb8a3cc9ef4a50
BT_1N3_25 <student@bths.edu>
2025-01-10 10:56:43 -0500

    changed main print

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("partner 1 changed this string :|");
         System.out.println("kaboom");
         System.out.println("insert text here");
         System.out.println("yippee");
