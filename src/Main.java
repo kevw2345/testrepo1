02ae5c6104a113bc5de8fad7655df79fab9e95c3
tacocatermelon <tacocatermelon24@gmail.com>
2025-01-10 10:54:22 -0500

    yippee added

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("kaboom");
         System.out.println("insert text here");
+        System.out.println("yippee");
     }
 }
